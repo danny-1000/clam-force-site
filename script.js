c772c2d4d9ddfcefb3e0da3f76c2cf1ac500096e
LoronDan <33015023+loron10@users.noreply.github.com>
2022-05-02 08:51:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ button.addEventListener("click", async () => {
   try {
     // Request the Bluetooth device through browser
     const device = await navigator.bluetooth.requestDevice({
-      optionalServices: ["00002aff-0000-1000-8000-00805f9b34fb", "device_information"],
+      optionalServices: ["000018ff-0000-1000-8000-00805f9b34fb", "device_information"],
       acceptAllDevices: true,
     });
 
@@ -15,7 +15,7 @@ button.addEventListener("click", async () => {
     const server = await device.gatt.connect();
 
     // Getting the services we mentioned before through GATT server
-    const batteryService = await server.getPrimaryService("00002aff-0000-1000-8000-00805f9b34fb");
+    const batteryService = await server.getPrimaryService("000018ff-0000-1000-8000-00805f9b34fb");
     const infoService = await server.getPrimaryService("device_information");
 
     // Getting the current battery level
