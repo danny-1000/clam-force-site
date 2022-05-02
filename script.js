6cc874ddd79d020a75055d0b2adc9a6a4255fb7c
LoronDan <33015023+loron10@users.noreply.github.com>
2022-05-02 12:49:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ button.addEventListener("click", async () => {
     
     // Convert recieved buffer to number
     const batteryLevel = await batteryLevelCharacteristic.readValue();
-    const batteryPercent = await batteryLevel.getUint8(20);  
+    const batteryPercent = await batteryLevel.getUint8(15);  
     
    // console.log( batteryLevel.array.ge);
 
