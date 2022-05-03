d4db8b3937e4da93ee421f8007d1baf28b31b4bf
LoronDan <33015023+loron10@users.noreply.github.com>
2022-05-03 10:43:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -26,8 +26,9 @@ button.addEventListener("click", async () => {
     // Convert recieved buffer to number
     const batteryLevel = await batteryLevelCharacteristic.readValue();
     const batteryPercent = (await batteryLevel.getUint8(0) + await batteryLevel.getUint8(1));  
+    console.log(batteryPercent);
     const pressure = parseInt(batteryPercent, 16);
-    
+    console.log(pressure);
     for(let i=0; i < 2; i++){
       console.log(i);
       console.log(await batteryLevel.getUint8(i));
