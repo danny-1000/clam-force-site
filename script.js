010e00a2d337912d81ee501b54659a1ea79b3561
LoronDan <33015023+loron10@users.noreply.github.com>
2022-05-17 14:11:55 -0700

    add battery level

--- script.js
+++ script.js
@@ -20,14 +20,23 @@ button.addEventListener("click", async () => {
 
     // Getting the services we mentioned before through GATT server
     
-    
+    const battery_service = await server.getPrimaryService("battery_service");
     const pressureService = await server.getPrimaryService("cc4a6a80-51e0-11e3-b451-0002a5d5c51b");
   //  const infoService = await server.getPrimaryService("device_information");
 
     // Getting the current battery level
+    const batteryLevelCharacteristic = await battery_service.getCharacteristic(
+        "battery_level"
+    )
     const pressureCharacteristic = await pressureService.getCharacteristic(
       "835ab4c0-51e4-11e3-a5bd-0002a5d5c51b"
     );
+      // Convert recieved buffer to number
+      const batteryLevel = await batteryLevelCharacteristic.readValue();
+      const batteryPercent = await batteryLevel.getUint8(0);
+      var element = document.getElementById("printBatteryPercent");
+      element.style.fontSize = "60px"; 
+      document.getElementById('printBatteryPercent').innerHTML = 'Battery='+batteryPercent+'%';
     var element = document.getElementById("printPressure");
         element.style.fontSize = "20px";
     let x=0;
@@ -41,11 +50,9 @@ button.addEventListener("click", async () => {
     document.body.style.fontSize="60px";
     const clampForce=1.477*pressure-428;
     document.getElementById('printForce').innerHTML = 'Force='+clampForce.toFixed(0) + ' lb';
-
     document.getElementById('printPressure').innerHTML = pressure + ' psi' ;
-
-    }
-    // Convert recieved buffer to number
+   }
+    
     
     /* 
    // console.log(pressure);
