6e2a9c41bd03118b250f3c79eea1015d957249c3
LoronDan <33015023+loron10@users.noreply.github.com>
2022-05-23 15:36:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -64,10 +64,10 @@ button.addEventListener("click", async () => {
     var pressure = parseInt(str,16);
     if(pressure>3000){pressure=0;}
     document.body.style.fontSize="60px";
-    const clampForce=(1.477*pressure-428)*info[2]/100;
-    //if(clampForce<200){
-    //  clampForce=0;
-  //  }
+   var clampForce=(1.477*pressure-428)*info[2]/100;
+    if(clampForce<0){
+      clampForce=0;
+    }
     document.getElementById('serialNumber').innerHTML = info[0];
     document.getElementById('printForce').innerHTML = 'Force='+clampForce.toFixed(0) + ' lb';
     document.getElementById('printPressure').innerHTML = pressure + ' psi' ;
