29e145518944541bbf679d6ef2dc4b4a5e0e9ff3
LoronDan <33015023+loron10@users.noreply.github.com>
2022-05-31 08:00:53 -0700

    version 5.2

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const button2 = document.getElementById("exit");
 let x=0;
 button2.addEventListener("click", function(){
 location.reload();
+console.log('stop');
 },false);
 
 button.addEventListener("click", async () => {
