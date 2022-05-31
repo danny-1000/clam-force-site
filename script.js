b5125b3e87d8417824757331cbcd4e8bcd40dac3
LoronDan <33015023+loron10@users.noreply.github.com>
2022-05-31 08:33:50 -0700

    version 5.4

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const button = document.getElementById("getDetails");
 //const details = document.getElementById("details");
 const button2 = document.getElementById("exit");
 let x=0;
-document.getElementById('button2').style.background='#000000';
+document.getElementById('button2').style.background='gray';
 button2.addEventListener("click", function(){
 location.reload();
 console.log('stop');
