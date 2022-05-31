3f46a2e679a1f63465fde0d1c557949580a15b1f
LoronDan <33015023+loron10@users.noreply.github.com>
2022-05-31 14:04:08 -0700

    version 6.0

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ button.addEventListener("click", async () => {
     // Request the Bluetooth device through browser
     let options = {
       filters:[
-     //  {namePrefix: '2'}
+       {namePrefix: '2'}
 
       ],
       optionalServices: ['battery_service','device_information','cc4a6a80-51e0-11e3-b451-0002a5d5c51b']
