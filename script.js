e4fefe9c5e0117ca9b653be003dd5f694373370f
LoronDan <33015023+loron10@users.noreply.github.com>
2023-12-04 15:58:46 -0800

    version 9.0

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ button.addEventListener("click", async () => {
   
     if(pressure>3000){pressure=0;}  //was pressure=3000
     if(pressure<400)
-    {pressure=pressOld;           //was pressure=pressOld
+    {pressure=0;           //was pressure=pressOld
     // pressDisplay=0;
      pressCount=2;        //reset when arms open
     // clampForce=0;
