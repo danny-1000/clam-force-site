5dc2577ae917beab28b13dc5a4a6750205f756cc
LoronDan <33015023+loron10@users.noreply.github.com>
2023-12-12 13:05:58 -0800

    wider rect

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const button2 = document.getElementById("exit");
 const myCanvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
 ctx.fillStyle = "blue";
-ctx.fillRect(20,20, 300,100);
+ctx.fillRect(20,20, 350,100);
 
 let x=0;
 let pressCount=2;
