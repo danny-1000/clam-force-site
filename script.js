90d0965006090d469aa3c844f2abacd9cdf1bd12
LoronDan <33015023+loron10@users.noreply.github.com>
2024-01-08 12:54:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ button.addEventListener("click", async () => {
        else {
       ctx.fillStyle = "blue";
       } 
-      ctx.fillRect(0,0, 250,250);
+      ctx.fillRect(0,0, 600,250);
   
    //}
    //else
