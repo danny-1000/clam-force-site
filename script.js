96bca26822e0fed75e76f1054f7274a9afa5917f
LoronDan <33015023+loron10@users.noreply.github.com>
2024-01-09 10:20:17 -0800

    changed text size

--- script.js
+++ script.js
@@ -108,8 +108,8 @@ button.addEventListener("click", async () => {
     pressOld=pressure;
     document.body.style.fontSize="30px";
     const element = document.getElementById('printForce');
-    element.style.fontSize = "80px";
-    document.getElementById('printForce').innerHTML = '  ' + clampForce.toFixed(0);
+    element.style.fontSize = "120px";
+    document.getElementById('printForce').innerHTML = '-----' + clampForce.toFixed(0);
     
     //document.getElementById('printBatteryPercent').innerHTML = 'Battery='+batteryPercent+'%';
    // document.getElementById('serialNumber').innerHTML = info[0];
