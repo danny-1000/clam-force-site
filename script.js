768b0330d824af2616cdb8b9b1e9d53751e0b6d9
LoronDan <33015023+loron10@users.noreply.github.com>
2024-01-18 12:31:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ button.addEventListener("click", async () => {
    // const str = new String(number0.toString(16) + number1.toString(16));
    // var pressure = parseInt(str,16);         //convert to integer
    
-   // if(pressure>3000){pressure=0;}  //was pressure=3000
+    if(pressure>3000){pressure=0;}  //was pressure=3000
    // if(pressure<300)
    // {pressure=0;           //was pressure=pressOld
     // pressDisplay=0;
@@ -95,7 +95,7 @@ button.addEventListener("click", async () => {
    // {
    // pressDisplay=pressure;         pressDisplay not neede use pressure
     //try{
-      document.getElementById('printForce').innerHTML = 'calculating';
+      document.getElementById('printForce').innerHTML = '*calculating';
     await sleep(2000);
     clampForce=(.0586*(pressure**1.3934))*info[2]/100; //clampForce=(1.3537*pressure-310)*info[2]/100;
    // } catch(error) {alert("error calculating force")}
