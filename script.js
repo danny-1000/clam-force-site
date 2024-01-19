c37e79bd0dc54d4e59b0dc802d2471a5fdfcbe77
LoronDan <33015023+loron10@users.noreply.github.com>
2024-01-19 08:50:59 -0800

    Update script.js

--- script.js
+++ script.js
@@ -73,11 +73,11 @@ button.addEventListener("click", async () => {
     { 
     document.getElementById('printForce').innerHTML = 'calculating';
     await sleep(1000);
-    const press= await pressureCharacteristic.readValue();
-        number0= await press.getUint8(0);
-        number1= await press.getUint8(1); 
-    const str = new String(number0.toString(16) + number1.toString(16));
-    var pressure = parseInt(str,16);         //convert to integer
+    const pressure= await pressureCharacteristic.readValue(getUint8);
+      //  number0= await press.getUint8(0);
+      //  number1= await press.getUint8(1); 
+  //  const str = new String(number0.toString(16) + number1.toString(16));
+   // var pressure = parseInt(str,16);         //convert to integer
    
     if(pressure>3000){pressure=0;}  //was pressure=3000
     if(pressure<300)
