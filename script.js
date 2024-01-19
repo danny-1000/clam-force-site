3bb623e665e739412516e94e4cb88671699753b9
LoronDan <33015023+loron10@users.noreply.github.com>
2024-01-19 12:02:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -63,14 +63,14 @@ button.addEventListener("click", async () => {
            number1= await press.getUint8(1); 
        const str = new String(number0.toString(16) + number1.toString(16));
        var pressure = parseInt(str,16);         //convert to integer
-       //if(pressure<300 || pressure>3000)
-       //{
-       // i--;
-      // }
-       //else{
+       if(pressure<300 || pressure>3000)
+       {
+        i=i-1;
+       }
+       else{
         ptemp=ptemp+pressure;
-       i++;
-       //}
+       i=i+1;
+       }
        
       }
       pressure=ptemp/3;  // average 3 readings
