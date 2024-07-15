43dcfa92914fe83429ce2c314ea1c36ef4280485
LoronDan <33015023+loron10@users.noreply.github.com>
2024-07-15 11:43:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ button.addEventListener("click", async () => {
      pressure=0;
     }         
 
-    clampForce=((.0007*(pressure**2))+(.9194*pressure)-94)*info[2]/100; //clampForce=.0586*((pressure**1.3934))*info[2]/100;  
+    clampForce=(((.0007*(pressure**2))+(.9194*pressure)-94)*.81)*info[2]/100; //clampForce=.0586*((pressure**1.3934))*info[2]/100;  
    // await sleep(3000); 
   }
     // Check if clampForce is low medium or high
